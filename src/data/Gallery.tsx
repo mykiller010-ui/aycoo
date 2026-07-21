1b7ebdd978e6a9733d0b8ee43347d56a0c8c8ca2
mykiller010-ui <mykiller010@gmail.com>
2026-07-21 03:30:28 +0300

    Add files via upload

---
+++ src/data/Gallery.tsx
@@ -0,0 +1,9 @@
+import img1 from "@/assets/gallery/1.jpg";
+import img2 from "@/assets/gallery/2.jpg";
+import img3 from "@/assets/gallery/3.jpg";
+
+export const gallery = [
+  img1,
+  img2,
+  img3,
+];
