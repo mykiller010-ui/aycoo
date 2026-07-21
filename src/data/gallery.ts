1bda862f99b30fdccf027530cee438eaa9e57085
mykiller010-ui <mykiller010@gmail.com>
2026-07-21 03:29:12 +0300

    Update gallery.ts

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,15 +1,39 @@
-import { gallery } from "@/data/gallery";
+import img1 from "@/assets/gallery/1.jpg";
+import img2 from "@/assets/gallery/2.jpg";
+import img3 from "@/assets/gallery/3.jpg";
+import img4 from "@/assets/gallery/4.jpg";
+import img5 from "@/assets/gallery/5.jpg";
+import img6 from "@/assets/gallery/6.jpg";
 
-export default function Gallery() {
-  return (
-    <div className="grid grid-cols-3 gap-4 p-8">
-      {gallery.map((image, i) => (
-        <img
-          key={i}
-          src={image}
-          className="aspect-square object-cover rounded-xl hover:scale-105 transition"
-        />
-      ))}
-    </div>
-  );
-}
+export const gallery = [
+  {
+    image: img1,
+    title: "Sunset",
+    date: "July 2026",
+  },
+  {
+    image: img2,
+    title: "City Lights",
+    date: "July 2026",
+  },
+  {
+    image: img3,
+    title: "Portrait",
+    date: "June 2026",
+  },
+  {
+    image: img4,
+    title: "Night",
+    date: "June 2026",
+  },
+  {
+    image: img5,
+    title: "Travel",
+    date: "May 2026",
+  },
+  {
+    image: img6,
+    title: "Memories",
+    date: "May 2026",
+  },
+];
